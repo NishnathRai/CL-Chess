f84ab8d08f6c7f47c50b541c1690ad5c5aff0dd1
Nishnath Rai <nishnathnishu1122@gmail.com>
2025-01-30 23:48:33 +0530

    fixed you killing you (move)

--- functions/askUserToGiveInput.py
+++ functions/askUserToGiveInput.py
@@ -15,6 +15,10 @@ def checkToBox(f,board,turn):
     if( x>7 or y>7 ):
         print("\t\t\t\t ⚠️ Above 7 is Not Possible")
         return False
+    #a user can kill him self
+    if( (board[x][y][0]=="W" and turn=="White") or (board[x][y][0]=="B" and turn=="Black") ):
+        print("\t\t\t\t ⚠️ Please Pic the right Coin (You cannot Kill You)")
+        return False
     
     #If all above cases are passed then we did it
     return True
@@ -50,7 +54,7 @@ def checkFromBox(f,board,turn):
 
 def askUserToGiveInput(turn,board):
     print(f"\t\t\t\t Its {turn} 's { "♚" if turn=="White" else  "♔" }  turn")
-    print("\t\t\t\t Enter row and colloum num without space eg:'8e' ",end="\n\n")
+    print("\t\t\t\t Enter row and colloum num without space ","eg:'87' 8 th Row and 7 th Colloum",sep="\n\t\t\t\t ",end="\n\n")
     fromIsPossible = False
     while not fromIsPossible:
         f = input("\t\t\t\t From -->  ")
